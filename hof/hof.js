c887ce7d826183f171a153e46583cfb39532655c
Amul <amulagrawal32@gmail.com>
2020-12-20 17:58:24 +0530

    higher order function

---
+++ hof/hof.js
@@ -0,0 +1,31 @@
+function greet(name) {
+    console.log('Hello', name)
+}
+function createGreeter(greeting) {
+
+    function greet(name) {
+        // here we treat as if name is function
+        console.log(greeting, name())
+    }
+
+    return greet
+}
+
+function getName() {
+    return document.getElementById('namebox').value
+}
+
+let g1 = createGreeter('Good Morning')
+let g2 = createGreeter('Good Evening')
+
+// console.log(typeof g1,1)
+
+console.log(greet('Arnav'))
+console.log(greet('Prateek'),3)
+
+console.log(g1('Arnav'),4)
+console.log(g1('Prateek'),5)
+
+console.log(g2('Arnav'),6)
+console.log(g2('Prateek'),7)
+
