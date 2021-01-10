38309d55c591b6de9052fd560f2d67e546746d98
Amul <amulagrawal32@gmail.com>
2021-01-10 14:37:43 +0530

    add down and up button

--- To Do list/script.js
+++ To Do list/script.js
@@ -9,16 +9,45 @@ window.onload
             let item = document.createElement('li');
             item.textContent = text.value;
             list.appendChild(item);
+            let btn = document.createElement('button')
+            btn.textContent = 'X';
             text.value = "";
+            item.appendChild(btn);
+            let upBtn = document.createElement('button')
+            upBtn.textContent = '↑'
+            item.appendChild(upBtn);
+            let downBtn = document.createElement('button')
+            downBtn.textContent = '↓'
+            item.appendChild(downBtn);
+            btn.onclick = function(event)
+            {
+                let x = event.target;
+                console.log(x);
+                x.parentElement.remove();
+                
+            }
+            upBtn.onclick = function(event)
+            {
+                event.target.parentElement.parentElement.insertBefore(
+                   
+                    event.target.parentElement ,
+                    event.target.parentElement.previousElementSibling
+                )
+            }
+            downBtn.onclick = function(event)
+            {
+                event.target.parentElement.parentElement.insertBefore(
+                   event.target.parentElement.nextElementSibling,
+                    event.target.parentElement 
+                    
+                )
+            }
         }
         else {
             alert("You have to add something!")
         }
-        list.onclick = function(event)
-        {
-            let x = event.target;
-            x.remove();   
-        }
+       
+        
     }
 
 }
